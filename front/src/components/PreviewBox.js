27514bad61ae21e36a4e1f16c672189af321ea57
Assil Tekaya <tekayaassil8@gmail.com>
2025-05-12 02:11:53 +0100

    its workinggggg

--- front/src/components/PreviewBox.js
+++ front/src/components/PreviewBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PreviewBox = () => (
   <div style={{ border: '1px solid #ccc', margin: '20px 0', height: '400px' }}>
     <iframe
-      src="http://localhost:3000/filecontent"
+      src={`${process.env.REACT_APP_API_URL}/filecontent`}
       title="Live Preview"
       width="100%"
       height="100%"
